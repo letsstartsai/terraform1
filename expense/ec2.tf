4bd316253d11bfc5efe3919545bb47723278de60
lets start sai <info@sai.com>
2024-05-11 18:03:10 +0530

    about to commit

---
+++ expense/ec2.tf
@@ -0,0 +1,46 @@
+#resource <resource-type> <resource-name>
+resource "aws_instance" "db" {
+    count = length(var.instance_names)
+    ami = var.image_id 
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = var.instance_names[count.index] == "db" ? "t3.small" : "t3.micro"
+    #left side things are called as arguments, right side are values
+    tags = merge(
+        var.common_tags,
+        {
+            Name   = var.instance_names[count.index]
+            Module = var.instance_names[count.index]
+        }
+
+
+    )
+        
+    }
+
+
+resource "aws_security_group" "allow_ssh" {
+    name = var.sg_name
+    description = var.sg_description
+
+    #this is block
+    ingress {
+        from_port           = var.ssh_port
+        to_port             = var.ssh_port
+        protocol            = var.protocol
+        cidr_blocks         = var.allowed_cidr
+        ipv6_cidr_blocks    = ["::/0"]
+    }
+    egress {
+        from_port            = 0
+        to_port              = 0
+        protocol             = "-1" # -1 all protocols 
+        cidr_blocks          = var.allowed_cidr
+        
+    }
+
+    tags = {
+        Name = "allow_ssh"
+        createBy ="Sai"
+    }
+  
+}
