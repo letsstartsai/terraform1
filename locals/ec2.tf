c15e3f88e90221dd2307bf44c5a88071a084ae50
lets start sai <info@sai.com>
2024-05-12 23:46:18 +0530

    about to commit

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "db" {
     ami = local.ami_id
     vpc_security_group_ids = [local.sg_id]
     instance_type = local.instance_type
-    tags = locals.tags
+    tags = local.tags
 
   
 }
