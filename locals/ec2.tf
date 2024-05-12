d23805be87a18c076a86cbfe77e48c8920f1efea
lets start sai <info@sai.com>
2024-05-12 23:43:48 +0530

    about to commit

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "db" {
     
     ami = local.ami_id
     vpc_security_group_ids = [local.sg_id]
-    instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
+    instance_type = local.instance_type
     tags = locals.tags
 
   
--- locals/locals.tf
+++ locals/locals.tf
@@ -2,7 +2,8 @@ locals {
 
   ami_id = "ami-090252cbe067a9e58"
   sg_id  = "sg-02bce74bfbab75b07" # replace with your security group ID
-  instance_type  = "t3.micro"
+  #instance_type  = "t3.micro"
+  instance_type = var.instance_name == "db" ? "t3.small" : "t3.micro"
   tags={
     Name = "locals"
   }
